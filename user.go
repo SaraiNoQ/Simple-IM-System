0f2dcf1f3f23d25d92f074911566a0b017ca7b7a
SaraiNoQ <2994498974@qq.com>
2024-09-19 20:50:03 +0800

    feat: Define User Class

---
+++ user.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"net"
+)
+
+type User struct {
+	Name string
+	Addr string
+	C    chan string
+	conn net.Conn
+}
+
+// User Class init function
+func NewUser(conn net.Conn) *User {
+	userAddr := conn.RemoteAddr().String()
+
+	user := &User{
+		Name: fmt.Sprintf("客户: %s", userAddr),
+		Addr: userAddr,
+		C:    make(chan string),
+		conn: conn,
+	}
+
+	// Starting a goroutine to listenning user channel
+	go user.ListenMessage()
+
+	return user
+}
+
+// Listening User Channel, Once have message, Transmit to Connection
+func (this *User) ListenMessage() {
+	for {
+		msg := <-this.C
+		this.conn.Write([]byte(msg + "\n"))
+	}
+}
